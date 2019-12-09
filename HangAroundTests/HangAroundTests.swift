efb8214196855866eb1b686de44a28ba09d6022a
Jef Malfliet <jef.malfliet@student.hogent.be>
2019-12-09 13:57:55 +0100

    Initial Commit

---
+++ HangAroundTests/HangAroundTests.swift
@@ -0,0 +1,34 @@
+//
+//  HangAroundTests.swift
+//  HangAroundTests
+//
+//  Created by Jef Malfliet on 09/12/2019.
+//  Copyright © 2019 Jef Malfliet. All rights reserved.
+//
+
+import XCTest
+@testable import HangAround
+
+class HangAroundTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
